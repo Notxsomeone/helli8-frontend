37bfa5623153396844f2917eeeae0ec48f5ab103
unknown <imnotxsomeone@gmail.com>
2024-04-09 13:47:13 +0330

    Added coin icons + loading screen

--- index.js
+++ index.js
@@ -8,22 +8,29 @@ async function load(url) {
 	// Row generation
 	const coins = Object.keys(json[brokers[0]]);
 	let index = 0;
-	for (let i = 0; i < coins.length;) {
+	for (let i = 0; i < coins.length - 1; ) {
 		let row = document.createElement("tr");
-
+		let img = document.createElement("img");
+		img.classList.add('right-image');
+		img.width = 50 ; 
+		
 		for (let j = 0; j < brokers.length; j++) {
 			if(index == 4) {
 				i++;
 				index = 0;
 			}
+			img.src ="img/" + coins[i].toUpperCase() + ".png"
+
 			const prices = Object.values(json[brokers[j]][coins[i]]);
 			let cell = document.createElement("th");
 			cell.innerHTML = prices[index] ? prices[index] : "قیمت ناموجود";
 			row.appendChild(cell);
+
 		}
 
 		let cell = document.createElement("th");
 		let str = coins[i].toUpperCase() + '/';
+
 		switch(index) {
 			case 0:
 				str += 'IRR - Buy';
@@ -39,10 +46,14 @@ async function load(url) {
 			break;
 		}
 		cell.innerHTML = str;
+		cell.appendChild(img);
 		row.appendChild(cell);
-		
 		index++;
 		table.appendChild(row);
 	}
+	var element = document.getElementById("loading");
+	element.classList.remove("loading");
 }
+//load("http://192.168.100.47:5000");
 load("http://127.0.0.1:5000");
+;
