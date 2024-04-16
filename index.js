327b898521032a4ce252f9e2b8011fc8a1efdea5
unknown <imnotxsomeone@gmail.com>
2024-04-16 13:36:21 +0330

    ?

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 async function load(url) {
 	const res = await fetch(url);
 	const json = await res.json();
-	
+
 	let table = document.getElementById("table");
 	const brokers = Object.keys(json);
-	
+
 	// Row generation
 	const coins = Object.keys(json[brokers[0]]);
 	let index = 0;
-	for (let i = 0; i < coins.length - 1; ) {
+	for (let i = 0; i < coins.length - 1;) {
 		let row = document.createElement("tr");
 		row.classList.add('styled-table')
 		let img = document.createElement("img");
 		img.classList.add('right-image');
-		img.width = 50 ; 
-		
+		img.width = 50;
+
 		for (let j = 0; j < brokers.length; j++) {
-			if(index == 4) {
+			if (index == 4) {
 				i++;
 				index = 0;
 			}
-			img.src ="img/" + coins[i].toUpperCase() + ".png"
+			img.src = "img/" + coins[i].toUpperCase() + ".png"
 
 			const prices = Object.values(json[brokers[j]][coins[i]]);
 			let cell = document.createElement("th");
-			cell.innerHTML = prices[index] ? prices[index] : "قیمت ناموجود";
+
+			let priceStr =  "قیمت ناموجود"
+			if (prices[index]) {
+				priceStr = prices[index].toLocaleString();
+				index < 2 ? priceStr += 'ريال' : priceStr += '$';
+			}
+
+			cell.innerHTML = priceStr;
 			row.appendChild(cell);
 
 		}
@@ -32,19 +39,19 @@ async function load(url) {
 		let cell = document.createElement("th");
 		let str = coins[i].toUpperCase() + '/';
 
-		switch(index) {
+		switch (index) {
 			case 0:
 				str += 'IRR - Buy';
-			break;
+				break;
 			case 1:
 				str += 'IRR - Sell'
-			break;
+				break;
 			case 2:
 				str += 'USDT - Buy';
-			break;
+				break;
 			case 3:
 				str += 'USDT - Sell';
-			break;
+				break;
 		}
 		cell.innerHTML = str;
 		cell.appendChild(img);
@@ -54,6 +61,13 @@ async function load(url) {
 	}
 	var element = document.getElementById("loading");
 	element.classList.remove("loading");
+	console.log($(`img[src*="BTC"]`).parents('th'));
+	$(`img[src*="BTC"]`).parents('th').css("background-color", "#ed9b53");
+	$(`img[src*="USDT"]`).parents('th').css("background-color", "#16a085");
+	$(`img[src*="BTH"]`).parents('th').css("background-color", "#16a085");
+	$(`img[src*="USDT"]`).parents('th').css("background-color", "#16a085");
+
+
 }
 load("http://172.19.10.51:5000");
 // load("http://127.0.0.1:5000");
